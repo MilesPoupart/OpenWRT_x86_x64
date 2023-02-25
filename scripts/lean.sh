c0771b18e03ea36473a3c66752dcdfc182347af4
MilesPoupart <renjunyang007@126.com>
2023-02-25 12:12:47 +0800

    fix autotimeset

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -37,6 +37,7 @@ git clone --depth=1 https://github.com/sirpdboy/NetSpeedTest
 
 # Add luci-app-autotimeset
 git clone --depth=1 https://github.com/sirpdboy/luci-app-autotimeset
+sed -i "s/\"control\"/\"system\"/g" luci-app-autotimeset/luasrc/controller/autotimeset.lua
 
 # Add luci-app-ssr-plus
 git clone --depth=1 https://github.com/fw876/helloworld
