b017c865bf51b6e8bcd1a6c8a7dabb56b7c0cdfa
MilesPoupart <renjunyang007@126.com>
2024-04-18 23:45:08 +0800

    update

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -76,10 +76,12 @@ git clone --depth=1 https://github.com/sirpdboy/NetSpeedTest
 git clone --depth=1 https://github.com/sirpdboy/luci-app-autotimeset
 sed -i "s/\"control\"/\"system\"/g" luci-app-autotimeset/luasrc/controller/autotimeset.lua
 
-# Add dockerman
+# Add luci-app-dockerman
+rm -rf ../../customfeeds/luci/collections/luci-lib-docker
+rm -rf ../../customfeeds/luci/applications/luci-app-docker
 rm -rf ../../customfeeds/luci/applications/luci-app-dockerman
-git clone --depth=1 https://github.com/lisaac/luci-app-dockerman
 github_partial_clone lisaac luci-app-dockerman use_default_branch applications/luci-app-dockerman luci-app-dockerman
+github_partial_clone lisaac luci-lib-docker use_default_branch collections/luci-lib-docker luci-lib-docker
 
 # Add mosdns
 rm -rf ../../customfeeds/packages/net/mosdns
