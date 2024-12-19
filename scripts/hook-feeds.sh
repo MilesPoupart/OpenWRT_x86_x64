ecc3bafddee7b9248ff1a060d7c4a4ec9655d488
MilesPoupart <renjunyang007@126.com>
2024-12-19 11:02:39 +0800

    add missing packages

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -79,6 +79,10 @@ clone_and_cleanup "packages/net/adguardhome" "immortalwrt" "packages" "use_defau
 rm -rf luci/applications/luci-app-smartdns
 git clone https://github.com/pymumu/luci-app-smartdns luci/applications/luci-app-smartdns
 
+# add missing packages
+clone_and_cleanup "luci/applications/luci-app-wireguard" "MilesPoupart" "master" "applications/luci-app-wireguard" "luci/applications/luci-app-wireguard"
+clone_and_cleanup "luci/applications/luci-app-adbyby-plus" "MilesPoupart" "master" "applications/luci-app-adbyby-plus" "luci/applications/luci-app-adbyby-plus"
+
 popd
 
 # Set to local feeds
