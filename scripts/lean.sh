6f75e3caa9c5d629adb98a1c00aa1387ef11b597
MilesPoupart <renjunyang007@126.com>
2025-04-27 14:55:57 +0800

    taskplan: new package name

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -74,8 +74,9 @@ git clone --depth=1 https://github.com/sirpdboy/luci-app-partexp
 git clone --depth=1 https://github.com/sirpdboy/NetSpeedTest
 
 # Add luci-app-autotimeset
-git clone --depth=1 https://github.com/sirpdboy/luci-app-autotimeset
-sed -i "s/\"control\"/\"system\"/g" luci-app-autotimeset/luasrc/controller/autotimeset.lua
+rm -rf ../../customfeeds/luci/applications/luci-app-autotimeset
+git clone --depth=1 https://github.com/sirpdboy/luci-app-taskplan
+sed -i "s/\"control\"/\"system\"/g" luci-app-taskplan/luasrc/controller/taskplan.lua
 
 # Add luci-app-dockerman
 rm -rf ../../customfeeds/luci/collections/luci-lib-docker
