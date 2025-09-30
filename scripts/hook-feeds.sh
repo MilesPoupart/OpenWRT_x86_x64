bd86f20171e6ea93797e85c899a3f76033048ae7
MilesPoupart <renjunyang007@126.com>
2025-09-30 21:52:54 +0800

    zerotier: use immortal source

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -86,7 +86,7 @@ clone_and_cleanup "packages/net/adguardhome" "immortalwrt" "packages" "use_defau
 
 # Replace luci-app-zerotier
 clone_and_cleanup "luci/applications/luci-app-zerotier" "immortalwrt" "luci" "use_default_branch" "applications/luci-app-zerotier" "luci/applications/luci-app-zerotier"
-# clone_and_cleanup "packages/net/zerotier" "immortalwrt" "packages" "use_default_branch" "net/zerotier" "packages/net/zerotier"
+clone_and_cleanup "packages/net/zerotier" "immortalwrt" "packages" "use_default_branch" "net/zerotier" "packages/net/zerotier"
 
 # replace luci-app-smartdns
 rm -rf luci/applications/luci-app-smartdns
