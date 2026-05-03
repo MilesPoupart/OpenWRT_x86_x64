206f34c8a4f046082ca402b57128ce548e083863
MilesPoupart <renjunyang007@126.com>
2026-05-03 20:25:48 +0800

    chore(scripts): 清理旧版本 easytier 文件夹
    - 删除 customfeeds 目录下旧的 luci-app-easytier 应用文件夹
    - 删除 customfeeds 目录下旧的 easytier 网络包文件夹
    - 确保后续克隆的 easytier 仓库不会与旧文件产生冲突

--- scripts/immortal.sh
+++ scripts/immortal.sh
@@ -267,6 +267,8 @@ rm -rf ../../customfeeds/luci/applications/luci-app-smartdns
 git clone --depth=1 https://github.com/pymumu/luci-app-smartdns
 
 # easytier
+rm -rf ../../customfeeds/luci/applications/luci-app-easytier
+rm -rf ../../customfeeds/packages/net/easytier
 git clone --depth=1 https://github.com/EasyTier/luci-app-easytier.git
 
 # Add luci-app-wolplus
